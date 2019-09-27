b1092eaee3c6d6438370b2fbbf90c7633920473a
Dani Sim <dsim@quantopian.com>
2019-09-27 12:43:09 -0400

    Add rest of installs.

--- ci/travis/overwrite_requirements.sh
+++ ci/travis/overwrite_requirements.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-#     # On OSX, sed refuses to edit in place, so give it an empty extension for the rename.
-#     function sed_inplace() {
-#         sed -i '' "$@"
-#     }
-# else
-function sed_inplace() {
-    sed -i "$@"
-}
-# fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # On OSX, sed refuses to edit in place, so give it an empty extension for the rename.
+    function sed_inplace() {
+        sed -i '' "$@"
+    }
+else
+    function sed_inplace() {
+        sed -i "$@"
+    }
+fi
 
 sed_inplace "s/numpy==.*/numpy==$NUMPY_VERSION/" etc/requirements.txt
 sed_inplace "s/pandas==.*/pandas==$PANDAS_VERSION/" etc/requirements.txt
